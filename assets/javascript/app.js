08312f506aa81b28a77fd82bf96f2e868f118524
AnyaKilpatrick <hannakilpatrick@gmail.com>
2018-03-12 19:50:25 -0400

    modified button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,7 @@ gifMaker = {
     },
 
     addButton: function(){    //method to add new buttons
-        $("#add").on("click", function(event){ //as it's a submit button, this function will be executed when you press Enter too
+        $("#add").on("click", function(event){ //function will be executed when you press Enter too
             event.preventDefault();
             var wordLength = $("#gifInput").val().trim().length;
             var myInput = $("#gifInput").val().trim();
